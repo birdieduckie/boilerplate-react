ab21ca40eafabde11663622516fbb40aa7c071dc
Natasha S <vrthngcnts@gmail.com>
2022-07-04 20:38:27 +0300

    feat: hot reload + babel + etc

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('hello')
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+
+import { App } from './screens/App/App'
+
+const root = createRoot(document.getElementById('root'))
+
+root.render(<App />)
+
+module.hot.accept()
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,38 @@
 const path = require('path')
+const webpack = require('webpack')
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
   entry: path.resolve(__dirname, './src/index.js'),
   module: {
     rules: [
       {
-        test: /\.(js)$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ['babel-loader'],
       },
     ],
   },
   resolve: {
-    extensions: ['*', '.js'],
+    extensions: ['*', '.js', '.jsx'],
   },
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'bundle.js',
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Hello Webpack bundled JavaScript Project',
+      template: path.resolve(__dirname, './src/core/templates/index.html'),
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
   devServer: {
     static: path.resolve(__dirname, './dist'),
+    historyApiFallback: true,
   },
+  devtool: 'source-map',
 }
